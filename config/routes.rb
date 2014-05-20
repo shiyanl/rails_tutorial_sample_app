687ecb8f77b6abbf0bd7e9c00276e3b935f4138d
Shiyan LU <developer.shiyan.lu@gmail.com>
2014-05-20 16:33:18 +1000

    finish chapter 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 RailsTutorialSampleApp::Application.routes.draw do
 
+
+  get "users/new"
+
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
 
   #match '/', to: 'static_pages#home', via: 'get'
   #or

4bc31d7366d5273ff55064328043dddc5a4a9f7b
Shiyan LU <developer.shiyan.lu@gmail.com>
2014-05-20 21:47:02 +1000

    6.3.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,11 @@ class User < ActiveRecord::Base
 
 	validates :email, presence: true
 
-	 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
-
+	validates :password, length: { minimum: 6 }
+	#(Presence validations for the password and its confirmation are automatically added by has_secure_password.)
+	has_secure_password
 
 end

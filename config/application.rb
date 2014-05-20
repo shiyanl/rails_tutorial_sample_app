3f995f02d695706037b54814536862288be0bb8a
Shiyan LU <developer.shiyan.lu@gmail.com>
2014-05-20 14:58:54 +1000

    before add partials

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module RailsTutorialSampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    #for bootstrape
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
   end
 end

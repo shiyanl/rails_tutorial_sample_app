16cf82526efc2950147108726896dcd1b9df751f
Shiyan LU <developer.shiyan.lu@gmail.com>
2014-05-22 10:51:03 +1000

    8.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       # Handle a successful save.
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user

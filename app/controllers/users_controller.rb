4299aba258c965aa5236887898000c365719de56
Shiyan LU <developer.shiyan.lu@gmail.com>
2014-05-22 13:02:19 +1000

    10.3.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
 
   # GET 'users/1'
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -59,13 +60,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-    # Before filters
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+
 
     def correct_user
       @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ RailsTutorialSampleApp::Application.routes.draw do
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
 
+  resources :microposts, only: [:create, :destroy]
 
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
